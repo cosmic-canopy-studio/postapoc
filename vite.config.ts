f59cc4dff88a018054ae06da3799795d3828a944
Sam Biggins <sambiggins@gmail.com>
2023-02-26 21:14:36 -0800

    remove gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import { ghPages } from 'vite-plugin-gh-pages';
+// import { ghPages } from 'vite-plugin-gh-pages';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   base: '/postapoc',
-  plugins: [react(), ghPages(), eslintPlugin()],
+  plugins: [react(), eslintPlugin()], // ghPages() // uncomment to deploy to gh-pages
   server: { host: '0.0.0.0', port: 8000 }
 });
