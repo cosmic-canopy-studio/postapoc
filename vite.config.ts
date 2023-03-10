56b3690cba3818fb6c9326fb39193bca5e796dbd
Sam Biggins <sambiggins@gmail.com>
2023-03-09 20:02:18 -0800

    re-add ghpages on build
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-// import { ghPages } from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 
 export default defineConfig({
     base: '/postapoc',
-    plugins: [react(), eslintPlugin()], // ghPages() // uncomment to deploy to gh-pages
+    plugins: [react(), eslintPlugin(), ghPages()], // ghPages() // uncomment to deploy to gh-pages
     server: { host: '0.0.0.0', port: 8000 },
     resolve: {
         alias: {
