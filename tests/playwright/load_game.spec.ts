eb649410c5e365943ed0484e1ae99db259ebb1ee
Sam Biggins <sambiggins@gmail.com>
2023-04-01 13:29:11 -0700

    test: update playwright test
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- tests/playwright/load_game.spec.ts
+++ tests/playwright/load_game.spec.ts
@@ -1,11 +1,12 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('End-to-end tests for based game loading and entry', () => {
+test.describe('End-to-end tests for base game loading and entry', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4173');
   });
 
-  test('canvas loads', async ({ page }) => {
+  test('canvas loads, matches screenshot', async ({ page }) => {
+    await page.waitForLoadState('networkidle');
     await page.waitForSelector('canvas');
     await expect(page).toHaveScreenshot();
   });
