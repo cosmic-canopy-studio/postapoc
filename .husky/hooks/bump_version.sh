f93cf4fb41048fb5ad8816f30f48484840870bf3
Sam Biggins <sambiggins@gmail.com>
2023-04-01 23:21:28 -0700

    build: remove comment based minor and major version bumps
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- .husky/hooks/bump_version.sh
+++ .husky/hooks/bump_version.sh
@@ -3,18 +3,18 @@
 # Bump version based on commit message
 last_commit_message=$(git log --format=%B -n 1 HEAD)
 
-if echo "$last_commit_message" | grep -q 'bump major'; then
-  # bump major version
-  yarn version --major --no-commit-hooks --no-git-tag-version
-elif echo "$last_commit_message" | grep -q 'bump minor'; then
-  # bump minor version
-  yarn version --minor --no-commit-hooks --no-git-tag-version
-elif echo "$last_commit_message" | grep -q 'bump skip'; then
+if echo "$last_commit_message" | grep -q 'bump skip'; then
   echo "Skipping version bump"
 else
   # bump patch version
   yarn version --patch --no-commit-hooks --no-git-tag-version
 fi
+
+# Get the new version number
+new_version=$(jq -r .version package.json)
+
+# Create a tag with the new version number
+git tag "v$new_version"
 
 # Commit the version bump
 git add package.json
--- COMMITLOG.md
+++ COMMITLOG.md
@@ -1,4 +1,6 @@
-## [0.1.0](https://github.com/Unnamed-GameDev-Studio/postapoc/compare/0.0.80...0.1.0) (2023-04-02)
+## [0.1.2](https://github.com/Unnamed-GameDev-Studio/postapoc/compare/0.0.89...v0.1.2) (2023-04-02)
+
+## [0.0.89](https://github.com/Unnamed-GameDev-Studio/postapoc/compare/0.0.80...0.0.89) (2023-04-02)
 
 
 ### Features
