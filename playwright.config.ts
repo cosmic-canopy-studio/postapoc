04bbca49695d9eef165fd970e2a6b1bb423bf4ca
Sam Biggins <sambiggins@gmail.com>
2023-04-02 22:48:44 -0700

    fix: playwright screenshot
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'html' : 'line',
+  reporter: process.env.CI ? 'github' : 'list',
   use: {
     baseURL: 'http://127.0.0.1:4173',
     trace: 'on-first-retry',
