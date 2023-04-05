2b66897787adbbc11425fd67c004c5cffa66b706
Sam Biggins <sambiggins@gmail.com>
2023-04-04 22:49:17 -0700

    ci: update base path to /postapoc/ to support gh-pages build
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,34 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import path from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  base: '/postapoc/',
   build: {
-    outDir: "dist"
+    outDir: 'dist',
   },
   server: {
     port: 3000,
-    strictPort: true
+    strictPort: true,
   },
   preview: {
     port: 4173,
-    strictPort: true
+    strictPort: true,
   },
   resolve: {
     alias: {
-      "@src": path.resolve(__dirname, "src")
-    }
+      '@src': path.resolve(__dirname, 'src'),
+    },
   },
   plugins: [
     viteStaticCopy({
       targets: [
         {
-          src: "assets/*",
-          dest: "assets"
-        }
-      ]
-    })
-  ]
+          src: 'assets/*',
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
 });
