0aa1caa1a7a638e3b7844481dec0f792b7e0a8c7
Sam Biggins <sambiggins@gmail.com>
2023-04-05 19:07:22 -0700

    feat: display current time state
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- src/phaser/scenes/mainScene.ts
+++ src/phaser/scenes/mainScene.ts
@@ -1,12 +1,14 @@
 // Part: src/phaser/scenes/mainScene.ts
-// Code Reference:
-// Documentation:
+// Code Reference: https://github.com/photonstorm/phaser
+// Documentation: https://photonstorm.github.io/phaser3-docs/
 
 import Universe from '@src/core/systems/universe';
 import Phaser from 'phaser';
+import { TimeState } from '@src/core/systems/timeSystem';
 
 export default class MainScene extends Phaser.Scene {
   private universe!: Universe;
+  private timeStateText!: Phaser.GameObjects.Text;
 
   constructor() {
     super('MainScene');
@@ -14,10 +16,18 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.initSystems();
+    this.timeStateText = this.add.text(this.cameras.main.width - 200, 10, '', {
+      fontSize: '24px',
+      color: '#ffffff',
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+      padding: { left: 10, right: 10, top: 5, bottom: 5 },
+    });
+    this.timeStateText.setScrollFactor(0);
   }
 
   update(time: number, deltaTime: number) {
     this.universe.update(time, deltaTime);
+    this.updateTimeStateText();
   }
 
   private initSystems() {
@@ -27,5 +37,10 @@ export default class MainScene extends Phaser.Scene {
     this.universe.generateStaticObject(200, 200, 'tree');
     this.universe.generateStaticObject(400, 400, 'bench');
     this.universe.spawnPlayer();
+  }
+
+  private updateTimeStateText() {
+    const timeState = this.universe.getTimeState();
+    this.timeStateText.setText(`${TimeState[timeState].toLowerCase()}`);
   }
 }
