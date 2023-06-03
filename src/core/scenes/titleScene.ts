833aa4224e2e790b0999a12fbcc2fd7e8ce00359
Sam Biggins <sambiggins@gmail.com>
2023-06-03 12:44:29 -0700

    fix: fix title screen mushroom cloud

--- src/core/scenes/titleScene.ts
+++ src/core/scenes/titleScene.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 
 export default class TitleScene extends Phaser.Scene {
+  private started = false;
   constructor() {
     super('TitleScene');
   }
@@ -9,11 +10,10 @@ export default class TitleScene extends Phaser.Scene {
     this.add.image(400, 300, 'starry_night');
 
     const forestSilhouette = this.add.image(400, 300, 'forest_silhouette');
-    forestSilhouette.setScale(1);
+    forestSilhouette.setScale(1).setDepth(10);
 
     const title = this.add.image(400, 150, 'postapoc_title');
-    title.setScale(2);
-
+    title.setScale(2).setDepth(10);
     this.input.on('pointerdown', () => this.startMainScene());
     if (!this.input.keyboard) {
       throw new Error('No keyboard found');
@@ -22,18 +22,24 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   private async startMainScene() {
-    await this.growMushroomCloud();
-    this.scene.start('MainScene');
+    if (!this.started) {
+      this.started = true;
+      await this.growMushroomCloud();
+      this.scene.start('MainScene');
+    }
   }
 
   private async growMushroomCloud() {
     return new Promise<void>((resolve) => {
-      const mushroomCloud = this.add.image(400, 300, 'mushroom_cloud');
-      mushroomCloud.setScale(0.1).setDepth(1);
-      mushroomCloud.setOrigin(mushroomCloud.originX, mushroomCloud.height);
+      const mushroomCloud = this.add.image(
+        400,
+        this.cameras.main.height,
+        'mushroom_cloud'
+      );
+      mushroomCloud.setScale(0.1).setOrigin(0.5, 1);
       this.tweens.add({
         targets: mushroomCloud,
-        scale: 20,
+        scale: 5,
         duration: 2000,
         ease: 'Sine.easeInOut',
         onComplete: () => {
@@ -43,15 +49,16 @@ export default class TitleScene extends Phaser.Scene {
 
       // Add the screen overlay
       const orangeRect = this.add.rectangle(400, 300, 800, 600, 0xff6600, 0);
-      orangeRect.setDepth(2);
+      orangeRect.setDepth(20);
 
       const blackRect = this.add.rectangle(400, 300, 800, 600, 0x000000, 0);
-      blackRect.setDepth(3);
+      blackRect.setDepth(30);
 
       this.tweens.add({
         targets: orangeRect,
         fillAlpha: 0.5,
         duration: 800,
+        delay: 400,
         ease: 'Linear',
         onComplete: () => {
           this.tweens.add({
