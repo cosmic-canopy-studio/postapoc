63c1aa25101514c34843e90002036a038ffd981e
Sam Biggins <sambiggins@gmail.com>
2023-06-29 16:36:25 -0700

    feat(assets): support tilemaps, resource replacement, tiles as sprites
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- COMMITLOG.md
+++ COMMITLOG.md
@@ -1,4 +1,4 @@
-## 0.0.1 (2023-06-28)
+## 0.0.1 (2023-06-29)
 
 
 ### Features
@@ -10,6 +10,7 @@
 * add config with debug toggle, ability to skip intro ([020d3e6](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/020d3e69bc2e5116246079cb37b04d47c5bd92d1))
 * add placeholder image for missing assets ([c5db639](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/c5db639252d44bccd2b6f419fccd76ab5aae2d81))
 * add title screen ([4bc4a62](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/4bc4a6217db44bb4f2156251dbd882c0322b9c41))
+* **assets:** add sample tileset ([5e45668](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/5e45668d1a9fe77684d9de7ca9f1adc611ac4db5))
 * **biome:** biome and overworld generation ([#37](https://github.com/Unnamed-GameDev-Studio/postapoc/issues/37)) ([33b9d57](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/33b9d57c6bd419f03dc216772f06d6d89f026cec))
 * collider ([57609dc](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/57609dccc238981e0f2f2b79efa9fd128a2c3e1e))
 * collider ([59f99e3](https://github.com/Unnamed-GameDev-Studio/postapoc/commit/59f99e3245e458d61d063af0db947cdc5ac49afe))
--- src/core/scenes/bootScene.ts
+++ src/core/scenes/bootScene.ts
@@ -3,8 +3,11 @@ import menuAssets from '@src/core/assets/prototype/menuAssets.json';
 import objectAssets from '@src/core/assets/prototype/objectAssets.json';
 import terrainAssets from '@src/core/assets/prototype/terrainAssets.json';
 import uiAssets from '@src/core/assets/prototype/uiAssets.json';
+import tilesetAssets from '@src/core/assets/kennynl/tilesetAssets.json';
+
 import { createFallbackSVG } from '@src/core/utils/svgUtils';
 import Phaser from 'phaser';
+import logger from '@src/telemetry/systems/logger';
 
 export default class BootScene extends Phaser.Scene {
   private progressBar!: Phaser.GameObjects.Graphics;
@@ -43,19 +46,76 @@ export default class BootScene extends Phaser.Scene {
       },
       this
     );
+
+    // Load other assets
     this.loadAssets(itemAssets);
     this.loadAssets(objectAssets);
     this.loadAssets(terrainAssets);
     this.loadAssets(menuAssets);
     this.loadAssets(uiAssets);
+    this.loadTilesetAssets(tilesetAssets);
   }
 
   create() {
+    tilesetAssets.forEach((asset) => {
+      const tilesetJson = this.cache.json.get(asset.key + 'Tilemap');
+      const image = this.textures.get(asset.key).getSourceImage();
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const context = canvas.getContext('2d');
+      context.drawImage(image, 0, 0);
+
+      console.log(tilesetJson);
+
+      tilesetJson.tilesets.forEach((tileset) => {
+        tileset.tiles.forEach((tile) => {
+          if (
+            tile.properties &&
+            tile.properties.some((property) => property.name === 'type')
+          ) {
+            const typeProperty = tile.properties.find(
+              (property) => property.name === 'type'
+            );
+            const x = (tile.id % tileset.columns) * tilesetJson.tilewidth;
+            const y =
+              Math.floor(tile.id / tileset.columns) * tilesetJson.tileheight;
+            const tileImageData = context.getImageData(
+              x,
+              y,
+              tilesetJson.tilewidth,
+              tilesetJson.tileheight
+            );
+            const tileCanvas = document.createElement('canvas');
+            tileCanvas.width = tilesetJson.tilewidth;
+            tileCanvas.height = tilesetJson.tileheight;
+            tileCanvas.getContext('2d').putImageData(tileImageData, 0, 0);
+
+            if (this.textures.exists(typeProperty.value)) {
+              logger.info(
+                `Removing existing texture ${typeProperty.value} in favor of preferred tileset texture`
+              );
+              this.textures.remove(typeProperty.value);
+            }
+
+            this.textures.addCanvas(typeProperty.value, tileCanvas);
+          }
+        });
+      });
+    });
+
     if (import.meta.env.DEV) {
       this.scene.start('MainScene');
     } else {
       this.scene.start('TitleScene');
     }
+  }
+
+  private loadTilesetAssets(assets: any[]) {
+    assets.forEach((asset) => {
+      this.load.image(asset.key, asset.url);
+      this.load.json(asset.key + 'Tilemap', asset.mapUrl);
+    });
   }
 
   private loadAssets(assets: any[]) {
